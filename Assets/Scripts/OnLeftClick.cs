efc7a115eec94517d8c80d00fb34dff80c86d352
Luke-png <lukas.jigberg@gmail.com>
2023-11-10 15:13:58 +0100

    My scene and click script

---
+++ Assets/Scripts/OnLeftClick.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+public class Clicker : MonoBehaviour
+{
+
+    [SerializeField] private bool distanceCheck = false;
+    [SerializeField] private Camera mainCamera = null;
+
+    void OnMouseDown(){
+    if (!distanceCheck){
+        ChangeColor();
+    }
+    else if (DistanceCamera()){
+        ChangeColor();
+    }
+}
+
+    private void ChangeColor()
+    {
+        Renderer renderer = transform.GetComponent<Renderer>();
+        if (renderer.material.color == Color.red){
+            renderer.material.color = Color.blue;
+        }
+        else{
+            renderer.material.color = Color.red;
+        }
+    }
+
+    private bool DistanceCamera()
+    {
+        float distanceToCamera = Vector3.Distance(transform.position, mainCamera.transform.position);
+        return distanceToCamera < 20.0f;
+    }
+}
