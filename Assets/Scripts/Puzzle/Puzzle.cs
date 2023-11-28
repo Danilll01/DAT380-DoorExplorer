130529a1e914e49d9729adf1c0c4d4054f30581b
Daniel Persson <perssondaniel01@gmail.com>
2023-11-28 16:49:16 +0100

    Added puzzle

---
+++ Assets/Scripts/Puzzle/Puzzle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour
+{
+
+    public bool hasCube = false;
+    public bool hasTriangle = false;
+    public bool hasCylinder = false;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
