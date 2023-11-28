4e0455b36a275a372ec76a5887c1590fa69d19bb
Turbobus <59963570+Turbobus@users.noreply.github.com>
2023-11-28 18:23:32 +0100

    Added escape menu

--- Assets/Scripts/RestartGame.cs
+++ Assets/Scripts/RestartGame.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class RestartGame : MonoBehaviour
 {
+    [SerializeField] private GameObject exitMenu;
     
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Scene scene = SceneManager.GetActiveScene();
-            SceneManager.LoadScene(scene.name);
+            exitMenu.SetActive(!exitMenu.activeSelf);
+            
         }
+    }
+
+    public void ResetGame()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
