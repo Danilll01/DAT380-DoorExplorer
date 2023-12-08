efe06b973d79060a92cb92dd260a7e432d970377
jigberg <lukas.jigberg@gmail.com>
2023-12-08 13:45:39 +0100

    GUI

--- Assets/Scripts/UI & Controls/CanvasController.cs
+++ Assets/Scripts/UI & Controls/CanvasController.cs
@@ -7,7 +7,10 @@ public class CanvasOverlay : MonoBehaviour
 
     private GameObject landScape;
     private GameObject portrait;
-    
+    private GameObject settingsL;
+    private GameObject settingsP;
+    private bool keepSettings = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,8 @@ public class CanvasOverlay : MonoBehaviour
 
         landScape = transform.Find("Landscape").gameObject;
         portrait = transform.Find("Portrait").gameObject;
+        settingsL = landScape.transform.Find("SettingsMenu").gameObject;
+        settingsP = portrait.transform.Find("SettingsMenu").gameObject;
     }
 
     // Update is called once per frame
@@ -26,13 +31,42 @@ public class CanvasOverlay : MonoBehaviour
     {
         if (Screen.orientation == ScreenOrientation.Portrait || Screen.orientation == ScreenOrientation.PortraitUpsideDown)
         {
-            portrait.SetActive(true);
-            landScape.SetActive(false);
+            SetChildrenActive(portrait);
+            foreach (Transform child in landScape.transform)
+            {
+                child.gameObject.SetActive(false);
+            }
         }
         else
         {
-            portrait.SetActive(false);
-            landScape.SetActive(true);
+            SetChildrenActive(landScape);
+            foreach (Transform child in portrait.transform)
+            {
+                child.gameObject.SetActive(false);
+            }
         }
     }
+
+    private void SetChildrenActive(GameObject parent)
+    {
+        foreach (Transform child in parent.transform)
+        {
+            if(child == settingsL.transform || child == settingsP.transform)
+            {
+                if(settingsL.activeSelf || settingsP.activeSelf)
+                {
+                    child.gameObject.SetActive(true);
+                }
+                else
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+            else
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+    }
+
 }
