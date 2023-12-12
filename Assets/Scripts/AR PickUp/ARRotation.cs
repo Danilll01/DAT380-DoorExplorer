186447695b22a501ec2bb615dcc09af42df69625
Turbobus <59963570+Turbobus@users.noreply.github.com>
2023-12-12 10:21:19 +0100

    Rotation works from front direction

--- Assets/Scripts/AR PickUp/ARRotation.cs
+++ Assets/Scripts/AR PickUp/ARRotation.cs
@@ -17,12 +17,18 @@ public class ARRotation : MonoBehaviour
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             originalRot = transform.rotation;
+            modifierRot = Quaternion.identity;
             originalScreenPos = Input.GetTouch(0).position;
         }
 
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             Vector2 coordinateDiff = Input.GetTouch(0).position - originalScreenPos;
+            //modifierRot.eulerAngles = new Vector3(coordinateDiff.y/8, -coordinateDiff.x/8);
+            modifierRot = Quaternion.AngleAxis(coordinateDiff.y / 8, Vector3.right) *
+                          Quaternion.AngleAxis(-coordinateDiff.x / 8, Vector3.up);
+            transform.rotation =  modifierRot * originalRot;
+            
         }
         
     }
