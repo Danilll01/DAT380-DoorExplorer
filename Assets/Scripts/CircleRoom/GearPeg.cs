68b62406e0cc8e0ba630b02703a96249807653ec
Turbobus <59963570+Turbobus@users.noreply.github.com>
2023-12-22 01:37:02 +0100

    Gears can now spin

--- Assets/Scripts/CircleRoom/GearPeg.cs
+++ Assets/Scripts/CircleRoom/GearPeg.cs
@@ -3,15 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Collider))]
 public class GearPeg : MonoBehaviour
 {
     [SerializeField] private GearPeg nextPeg;
     [SerializeField] private GearPeg beforePeg;
+    [SerializeField] private PegType pegType = PegType.MIDDLE;
+    
+    private enum PegType
+    {
+        START,
+        MIDDLE,
+        END
+    }
 
     private int currentForce = 0;
     private float currentSpeed = 0f;
+    private float startSpeed = 0;
 
     private bool hasGear = false;
     private bool hasDropped = false;
@@ -19,32 +29,41 @@ public class GearPeg : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (pegType == PegType.START)
+        {
+            startSpeed = 30;
+            ReCalculateSpeed();
+            hasGear = true;
+        }
+        
+        if (pegType == PegType.END)
+        {
+            hasGear = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up, currentSpeed);
+        transform.Rotate(Vector3.up, currentSpeed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        print("TJENA!!!!!!!!!!!!!!!!!!!!!!!!!!: " + other.gameObject.layer);
-       
+        // Return if it is not the specific type of collider we look for
         if (other.GetType() != typeof(SphereCollider)) { return; }
         
+        // Return if the gear has been dropped right before
         if (hasDropped)
         { hasDropped = false; return;}
         
         GearScript gearScript = other.GetComponent<GearScript>();
         if (gearScript != null && !hasGear)
         {
-            print("Inne!!!!!!!!!!!!!!!!!!!");
             
             Transform transform1 = other.transform;
             Rigidbody gearBody = transform1.GetComponent<Rigidbody>();
-
-
+            
             hasGear = true;
             transform1.parent = transform;
             transform1.localPosition = Vector3.zero; 
@@ -53,7 +72,7 @@ public class GearPeg : MonoBehaviour
             
             if (beforePeg != null || nextPeg != null)
             {
-                beforePeg.AddCurrentForce(currentForce);
+                beforePeg.AddCurrentForce(currentForce, this);
                 nextPeg.SetNewSpeed(-currentSpeed);
             }
             
@@ -62,19 +81,18 @@ public class GearPeg : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        print("HEJDÅ");
         GearScript gearScript = other.GetComponent<GearScript>();
         if (gearScript != null && hasGear)
         {
             Transform transform1 = other.transform;
             Rigidbody gearBody = transform1.GetComponent<Rigidbody>();
-            if (gearBody.isKinematic) { return; }
             
-            print("HEJDÅ PÅ RIKTIGT DENNA GÅNG");
+            // We can not remove gear if it is supposed to be still
+            if (gearBody.isKinematic) { return; }
             
             if (beforePeg != null || nextPeg != null)
             {
-                beforePeg.AddCurrentForce( -currentForce );
+                beforePeg.AddCurrentForce(-currentForce, this);
                 nextPeg.SetNewSpeed(0);
             }
                 
@@ -85,15 +103,33 @@ public class GearPeg : MonoBehaviour
         }
     }
 
-    private void AddCurrentForce(int force)
+    private void AddCurrentForce(int force, GearPeg from)
     {
         currentForce += force;
-        beforePeg.AddCurrentForce(currentForce);
+        
+        if (pegType == PegType.MIDDLE)
+        {
+            beforePeg.AddCurrentForce(currentForce, this);
+        }
+
+        if (pegType == PegType.START)
+        {
+            ReCalculateSpeed();
+            from.SetNewSpeed(currentSpeed);
+        }
     }
 
     private void SetNewSpeed(float speed)
     {
-        currentSpeed = speed;
-        nextPeg.SetNewSpeed(-currentSpeed);
+        if (hasGear)
+        {
+            currentSpeed = speed;
+            nextPeg.SetNewSpeed(-currentSpeed);
+        }
+    }
+
+    private void ReCalculateSpeed()
+    {
+        currentSpeed = startSpeed - currentForce * 2;
     }
 }
