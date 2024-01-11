cc7990b2043f4f3ac45fe9d8e3f6b53ba8898a52
Daniel Persson <perssondaniel01@gmail.com>
2024-01-11 17:16:06 +0100

    Started with floor calibration

--- Assets/Scripts/PickUpAR.cs
+++ Assets/Scripts/PickUpAR.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using UnityEngine;
 using System;
+using System.Collections.Generic;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 
 // The tag item should add the required components, like outline and rigidbody
@@ -24,6 +27,8 @@ public class PickUpAR : MonoBehaviour
     private bool pickUpButtonClicked = false;
     private bool doorPlacingMode = false;
     private ArObjectPlacer arObjectPlacer;
+    [SerializeField] private ARRaycastManager arRaycastManager;
+    [SerializeField] private Transform floor;
     [SerializeField] private Transform doorFrameOutline;
     private Vector3 doorFrameOutlineOriginalPosition;
     [SerializeField] private Vector3 doorFrameOffset = new Vector3(0.1f, 0, 0);
@@ -272,7 +277,7 @@ public class PickUpAR : MonoBehaviour
             HandleDoorPlacement(startPos, direction);
             return;
         }
-        
+
         RaycastHit hit = SendRaycast(startPos, direction, length);
         if (hit.collider == null)
         {
@@ -438,6 +443,22 @@ public class PickUpAR : MonoBehaviour
             doorFrameOutline.position = doorFrameOutlineOriginalPosition;
             doorFrameOutline.rotation = Quaternion.Euler(0, 0, 0);
         }
+    }
+
+    private void HandleFloorPlacement()
+    {
+        
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        Vector2 screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        if (arRaycastManager.Raycast(screenCenter, hits, TrackableType.FeaturePoint))
+        {
+            Pose hitPose = hits[0].pose;
+            
+            // Calculate the y differance between the floor and the camera
+            float yDiff = hitPose.position.y - floor.position.y;
+            transform.parent.position -= new Vector3(0, yDiff, 0);
+        }
+        
     }
 
     public void PickupTouch()
